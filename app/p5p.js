d51968ce731faeb553b7c8fde61666871408927e
Nec Necsoft <necsoftdesign@gmail.com>
2014-09-19 20:37:07 -0300

    Eliminado un comentario al pedo

--- app/p5p.js
+++ app/p5p.js
@@ -54,14 +54,4 @@
        });
      });
    });
-
-   //  childProcess.exec('processing-java --sketch='+sketch_dir+' --output='+sketch_build+' --run --force ', function (error, stdout, stderr) {
-   //    if (error) {
-   //      console.log(error.stack);
-   //      console.log('Error code: '+error.code);
-   //      console.log('Signal received: '+error.signal);
-   //    }
-   //   //  console.log('Child Process STDOUT: '+stdout);
-   //   //  console.log('Child Process STDERR: '+stderr);
-   //  });
  };
