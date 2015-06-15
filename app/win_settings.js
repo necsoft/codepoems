5b5f72cd3a8657d4f8a95f6d66279f14583242db
necsoft <necsoftdesign@gmail.com>
2015-06-15 15:17:58 -0300

    Translated win_settings

--- app/win_settings.js
+++ app/win_settings.js
@@ -1,8 +1,16 @@
+/*
+  > win_settings.js
+  
+  Codepoems settings window
+
+  */
+
+// Dependencies
 var gui = require('nw.gui');
 var ui = require('./ui.js');
-
 var win = gui.Window.get();
 
+// Set the window as active
 global.app.settings_window_active = true;
 
 // Handle the close of the window
@@ -13,11 +21,15 @@ win.on('close', function() {
 });
 
 $(document).ready(function() {
+
+    // Set the default values
     var actual_font_size = global.app.settings.font_size;
     $(".default_template_textarea").val(global.app.settings.default_template);
     $(".font_size_input").val(global.app.settings.font_size);
     $(".select_theme").val(global.app.settings.theme);
 
+
+    // Handle plus button
     $(".plus_button").click(function() {
         if (actual_font_size < 32) {
             actual_font_size++;
@@ -25,6 +37,7 @@ $(document).ready(function() {
         }
     });
 
+    // Handle minus button
     $(".minus_button").click(function() {
         if (actual_font_size > 7) {
             actual_font_size--;
@@ -32,12 +45,14 @@ $(document).ready(function() {
         }
     });
 
+    // Select the theme
     $('.select_theme').on('change', function() {
         ui.actions_change_theme(this.value);
         global.app.settings.theme = this.value;
         ui.action_save_default_settings();
     });
 
+    // Change and save the font size
     function change_font_size() {
         $(".font_size_input").val(actual_font_size);
         global.app.settings.font_size = actual_font_size;
@@ -45,6 +60,7 @@ $(document).ready(function() {
         ui.action_save_default_settings();
     }
 
+    // Save the default template
     $(".save_default_template").click(function() {
         global.app.settings.default_template = $(".default_template_textarea").val();
         ui.action_save_default_settings();
